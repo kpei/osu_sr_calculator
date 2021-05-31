dfefab768c477b6803db2c2a2160eaf4268dcb58
Kevin <you@example.com>
2021-05-30 23:04:57 -0400

    base + aim skill

--- osu_sr_calculator/DifficultyHitObjectCreator.py
+++ osu_sr_calculator/DifficultyHitObjectCreator.py
@@ -3,7 +3,7 @@ from .Objects.osu.HitObjects.DifficultyHitObject import DifficultyHitObject
 from .Objects.osu.HitObjects.Slider import Slider
 from .Objects.osu.HitObjects.Spinner import Spinner
 from .Objects.Vector2 import Vector2
-from math import atan2
+from math import atan2, e, pi, sin
 
 class DifficultyHitObjectCreator(object):
     difficultyHitObjects = []
@@ -19,6 +19,12 @@ class DifficultyHitObjectCreator(object):
     Angle = 0
     DeltaTime = 0
     StrainTime = 0
+
+    FlowProbability = 0
+    DistanceVector = Vector2(0.0, 0.0)
+
+    def clamp(self, value, _min, _max):
+        return max(min(value, _max), _min)
 
     def convertToDifficultyHitObjects(self, hitObjects, timeRate):
         self.difficultyHitObjects = []
@@ -41,8 +47,9 @@ class DifficultyHitObjectCreator(object):
 
         self.setDistances()
         self.setTimingValues()
+        self.calculateFlowProbability()
 
-        return DifficultyHitObject(self.currentObject, self.lastObject, self.lastLastObject, self.TravelDistance, self.JumpDistance, self.Angle, self.DeltaTime, self.StrainTime)
+        return DifficultyHitObject(self.currentObject, self.lastObject, self.lastLastObject, self.TravelDistance, self.JumpDistance, self.Angle, self.FlowProbability, self.DistanceVector, self.DeltaTime, self.StrainTime)
 
     def setDistances(self):
         self.TravelDistance = 0
@@ -56,8 +63,8 @@ class DifficultyHitObjectCreator(object):
             self.currentObject.Radius = self.normalized_radius
 
         scalingFactor = self.normalized_radius / self.currentObject.Radius
-        if(self.currentObject.Radius < 30):
-            smallCircleBonus = min(30 - self.currentObject.Radius, 5) / 50
+        if(self.currentObject.Radius < 32):
+            smallCircleBonus = min(32 - self.currentObject.Radius, 5) / 50
             scalingFactor *= 1 + smallCircleBonus
         
         if(isinstance(self.lastObject, Slider)):
@@ -68,7 +75,8 @@ class DifficultyHitObjectCreator(object):
         lastCursorPosition = self.getEndCursorPosition(self.lastObject)
 
         if(not isinstance(self.currentObject, Spinner)):
-            self.JumpDistance = self.currentObject.StackedPosition.scale(scalingFactor).subtract(lastCursorPosition.scale(scalingFactor)).length()
+            self.DistanceVector = self.currentObject.StackedPosition.scale(scalingFactor).subtract(lastCursorPosition.scale(scalingFactor))
+            self.JumpDistance = self.DistanceVector.length()
         
         if(self.lastLastObject is not None):
             lastLastCursorPosition = self.getEndCursorPosition(self.lastLastObject)
@@ -80,6 +88,7 @@ class DifficultyHitObjectCreator(object):
             det = v1.x * v2.y - v1.y * v2.x
 
             self.Angle = abs(atan2(det, dot))
+            self.Angle = 0 if(self.Angle == None) else self.Angle
 
     def setTimingValues(self):
         self.DeltaTime = (self.currentObject.StartTime - self.lastObject.StartTime) / self.timeRate
@@ -123,3 +132,15 @@ class DifficultyHitObjectCreator(object):
             pos = obj.LazyEndPosition if obj.LazyEndPosition is not None else pos
 
         return pos
+    
+    def calculateFlowProbability(self):
+        deltaTime = self.DeltaTime
+        distance = self.JumpDistance
+        angle = self.Angle
+
+        angle = self.clamp(angle, pi / 6, pi / 2)
+        angleOffset = 10.0 * sin(1.5 * (pi / 2 - angle))
+        distanceOffset = distance ** 1.7 / 325
+
+        flow = self.clamp((deltaTime - 126.0 + distanceOffset + angleOffset), -1e2, 1e2)
+        self.FlowProbability = 1.0 / (1.0 + e ** flow)
--- osu_sr_calculator/StarRatingCalculator.py
+++ osu_sr_calculator/StarRatingCalculator.py
@@ -5,37 +5,29 @@ from math import ceil, sqrt
 
 class StarRatingCalculator(object):
     hitObjects = []
-    section_length = 400
-    difficlty_multiplier = 0.0675
+    difficultyMultiplier = 0.18
+    displayDifficultyMultiplier = 0.605
 
     def calculate(self, hitObjects, timeRate):
         self.hitObjects = hitObjects
         aimSkill = Aim()
         speedSkill = Speed()
 
-        sectionLength = self.section_length * timeRate
-
-        currentSectionEnd = ceil((self.hitObjects[0].StartTime if hitObjects[0] is not None else 0) / sectionLength) * sectionLength
-
         for h in hitObjects:
-            while(h.CurrentObject.StartTime > currentSectionEnd):
-                aimSkill.saveCurrentPeak()
-                aimSkill.startNewSectionFrom(currentSectionEnd)
-
-                speedSkill.saveCurrentPeak()
-                speedSkill.startNewSectionFrom(currentSectionEnd)
-
-                currentSectionEnd += sectionLength
-
             aimSkill.process(h)
-            speedSkill.process(h)
-        
-        aimSkill.saveCurrentPeak()
-        speedSkill.saveCurrentPeak()
+            # speedSkill.process(h)
 
-        aimRating = sqrt(aimSkill.difficultyValue()) * self.difficlty_multiplier
-        speedRating = sqrt(speedSkill.difficultyValue()) * self.difficlty_multiplier
-        starRating = aimRating + speedRating + abs(aimRating - speedRating) / 2
+        aimRating = (aimSkill.difficultyValue() ** 0.75) * self.difficultyMultiplier
+        speedRating = 0
+
+        displayAimRating = (aimSkill.calculateDisplayDifficultyValue() ** 0.75) * self.displayDifficultyMultiplier
+        displaySpeedRating = 0
+
+        displayAimPerformance = (5.0 * max(1.0, displayAimRating) - 4.0) ** 3.0 / 100000.0
+        displaySpeedPerformance = (5.0 * max(1.0, displaySpeedRating) - 4.0) ** 3.0 / 100000.0
+        totalpp = (displayAimPerformance ** 1.1 + displaySpeedPerformance ** 1.1) ** (1.0/1.1)
+
+        starRating = 0.027 * (((100000.0 / 2.0 ** (1.0 / 1.1) * totalpp) ** (1/3.0)) + 4.0)
         return {
             "aim": aimRating,
             "speed": speedRating,
