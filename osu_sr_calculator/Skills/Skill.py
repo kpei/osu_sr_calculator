e1d522d13df2ed337f37a8adfd6c13199f63a648
Kevin <you@example.com>
2021-05-31 18:14:13 -0400

    trim Skill.py

--- osu_sr_calculator/Skills/Skill.py
+++ osu_sr_calculator/Skills/Skill.py
@@ -8,27 +8,6 @@ class Skill(ABC):
     def __init__(self):
         self.Previous = []
 
-    def process(self, currentObject):
-        pass
-
-    def difficultyValue(self):
-        self.strainPeaks.sort(reverse=True)
-
-        difficulty = 0
-        weight = 1
-
-        for strain in self.strainPeaks:
-            difficulty += strain * weight
-            weight *= 0.9
-
-        return difficulty
-
-    def strainValueOf(self, currentObject):
-        pass
-
-    def strainDecay(self, ms):
-        return pow(self.StrainDecayBase, ms / 1000)
-
     def addToHistory(self, currentObject):
         self.Previous.insert(0, currentObject)
         if(len(self.Previous) > 2):
