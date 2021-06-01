e49b2c5214c95e03de1338cb8959057ba8a5313e
Kevin <you@example.com>
2021-06-01 19:52:07 -0400

    fix more basic calcs

--- osu_sr_calculator/Objects/Vector2.py
+++ osu_sr_calculator/Objects/Vector2.py
@@ -6,7 +6,13 @@ class Vector2(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
+    def __str__(self):
+        return f"<Vector2 ({self.x}, {self.y})>"
+
+    def __repr__(self):
+        return self.__str__()
+
     def add(self, vector):
         return Vector2(self.x + vector.x, self.y + vector.y)
     
